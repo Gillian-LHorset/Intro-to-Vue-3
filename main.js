41eeabc7bc965af881da535f28e8e006d89d662f
Gillian-LHorset <pq60soi@eduvaud.ch>
2026-01-13 14:20:39 +0100

    feat(introduction): Completion du premier défi - introduction 2.7

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 const product = 'Socks'
+const description = 'A sock'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: description
+        }
+    }
+})
