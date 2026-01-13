895c044ea093e013e7d6269119bb35749f061499
Gillian-LHorset <pq60soi@eduvaud.ch>
2026-01-13 14:38:40 +0100

    fix(introduction defi 3.7): Correction de l'exercice

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_green.jpg',
-            attribut: "https://www.etml.ch/"
+            url: "https://www.etml.ch/"
         }
     }
 })
