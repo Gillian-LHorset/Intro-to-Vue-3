d1ffd245e35d0d7f726a38b6e895132c4a545ec6
Gillian-LHorset <pq60soi@eduvaud.ch>
2026-01-20 14:12:33 +0100

    feat(introduction): Complétion du défi 10.4

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -39,6 +39,11 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+        <button 
+          class="button"
+          v-on:click="removeFromCart">
+          Remove Item
+        </button>
       </div>
     </div>
   </div>`,
@@ -55,28 +60,38 @@ app.component('product-display', {
     }
   },
   methods: {
-      addToCart() {
-          this.cart += 1
-      },
-      updateVariant(index) {
-          this.selectedVariant = index
-      }
+    addToCart() {
+        this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+    },
+    removeFromCart() {
+        this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
+    },
+    updateVariant(index) {
+        this.selectedVariant = index
+    },
+    
   },
-  computed: {
-      title() {
-          return this.brand + ' ' + this.product
-      },
-      image() {
-          return this.variants[this.selectedVariant].image
-      },
-      inStock() {
-          return this.variants[this.selectedVariant].quantity
-      },
-      shipping() {
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        shipping() {
         if (this.premium) {
-          return 'Free'
+            return 'Free'
         }
-        return 2.99
-      }
+            return 2.99
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
   }
 })
